897a3b330097625c89154ef6d82143ac17a9e606
Sean Park <seansangpark@gmail.com>
2021-11-13 14:05:58 -0800

    Created Modal component fot React Portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>)}
+    </>
+  );
+};
+
+export default Modal;
