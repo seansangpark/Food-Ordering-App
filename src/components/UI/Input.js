beda2143d4490fe208ed782a935be2ae1a1b6f55
Sean Park <seansangpark@gmail.com>
2021-11-13 13:14:55 -0800

    Adding a form and default input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import classes from './Input.module.css';
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      {/* assign type="text" automatically */}
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
